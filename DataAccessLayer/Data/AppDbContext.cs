ac0d661d699150f51ba0dcb47439dac1bd2b485f
kareem hamed <khidmt55@gmail.com>
2024-09-14 17:01:34 +0300

    Add project files.

---
+++ DataAccessLayer/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
+    }
+}
